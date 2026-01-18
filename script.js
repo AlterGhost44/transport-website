7337dd5377ce501e212c54db299f877dd6749316
AlterGhost44 <beniaminfirmowy1@outlook.com>
2026-01-18 15:39:06 +0100

    Mobile layout finalized. Start build desktop layout

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ burger.addEventListener('click', () => {
   menu.classList.toggle('active');
   burger.classList.toggle('active');
 });
+
+const closeBtn = document.getElementById('closeForm');
+const openBtn = document.getElementById('openForm');
+const formContainer = document.getElementById('form-section');
+
+openBtn.addEventListener('click', () => {formContainer.classList.replace('form_isHidden', 'form_isActive');});
+closeBtn.addEventListener('click', () => {formContainer.classList.replace('form_isActive', 'form_isHidden');});
